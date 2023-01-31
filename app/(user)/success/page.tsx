ec2a2fbf0202af8986d72fcbbba91c494852c29a
codetributor <okaclint@gmail.com>
2023-01-31 08:35:33 -1000

    payment successful

---
+++ app/(user)/success/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Success() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-gray-400 text-2xl">Payment Successful!</h1>
+    </div>
+  );
+}
+
+export default Success;
