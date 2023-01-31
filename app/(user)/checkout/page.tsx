face64c9f72d64665c138066f1bd8babf137b2f0
codetributor <okaclint@gmail.com>
2023-01-30 14:17:38 -1000

    added shopping cart

---
+++ app/(user)/checkout/page.tsx
@@ -0,0 +1,19 @@
+function Checkout() {
+  return (
+    <main className="lg-flex mx-auto">
+      <div className="flex-grow m-5 shadow-sm">
+        {/*left section*/}
+        <div className="flex flex-col p-5 space-y-10">
+          <h1 className="text-3xl text-gray-400 border-b pb-4">
+            Your Shopping Basket
+          </h1>
+        </div>
+      </div>
+
+      {/*right section*/}
+      <div></div>
+    </main>
+  );
+}
+
+export default Checkout;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,20 @@
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
+    <header className="sticky top-0 z-50 bg-white flex items-center justify-between space-x-2 font-bold px-10 py-5">
       <div className="flex items-center space-x-2">
         <Link href="/">
           <img src="/logo-whole-notes.png" alt="logo" height={60} width={250} />
         </Link>
       </div>
-      <div className="text-white space-x-4">
-        <button className="bg-blue-300 px-4 py-1 rounded-md">Login</button>
+      <div className="relative space-x-4">
+        <div className="absolute top-0 right-0 bg-yellow-300 rounded-full px-1">
+          <span className="text-gray-400">4</span>
+        </div>
+        <ShoppingCartIcon className="h-12 w-12 text-blue-300" />
       </div>
     </header>
   );
