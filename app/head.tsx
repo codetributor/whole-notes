d8e29ff952a98b599f4c7944e259fb96529a342d
codetributor <okaclint@gmail.com>
2023-01-31 15:56:08 -1000

    meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,18 @@ export default function Head() {
     <>
       <title>Whole Notes</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:url" content="whole-notes.org" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Whole Notes" />
+      <meta
+        name="twitter:card"
+        content="Buy one mini notebook and we donate one mini notebook to someone living with a mental health condition"
+      />
+      <meta
+        property="og:description"
+        content="Buy one mini notebook and we donate one mini notebook to someone living with a mental health condition"
+      />
+      <meta property="og:image" content={"/logo.png"} />
       <link rel="icon" href="/logo.png" />
     </>
   );
